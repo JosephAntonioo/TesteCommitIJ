13e656c7f48615c3ef910f36cb89373c8fdcbb25
José Favaro <jose-tof@hotmail.com>
2021-08-02 11:35:13 -0300

    FirstCommit

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+    public static void main(String[] args) {
+        Pessoas pessoas = new Pessoas();
+    }
+
+    public static class Pessoas{
+        public String nome;
+        public double preco;
+        public int quantidade;
+
+        public Pessoas() {
+        }
+
+        public Pessoas(String nome, double preco, int quantidade) {
+            this.nome = nome;
+            this.preco = preco;
+            this.quantidade = quantidade;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public double getPreco() {
+            return preco;
+        }
+
+        public void setPreco(double preco) {
+            this.preco = preco;
+        }
+
+        public int getQuantidade() {
+            return quantidade;
+        }
+
+        public void setQuantidade(int quantidade) {
+            this.quantidade = quantidade;
+        }
+    }
+}
